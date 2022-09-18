ebe2205481dc382c8df99e1e43ce5f658f193609
Matthew Guy Southerland <mgsoutherland529@gmail.com>
2022-09-17 22:15:30 -0400

    commented out debugger

--- app.py
+++ app.py
@@ -61,5 +61,5 @@ def update(id):
         return render_template("update.html", task=task)
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
